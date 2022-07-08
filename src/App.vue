745031c407c7f06252490ddc8ff34b4d1c026a45
zhLisha <lisazhou1709@gmail.com>
2022-07-08 13:56:41 +0200

    Header done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <HeaderSpotify />
+    <header>
+      <HeaderSpotify />
+    </header>
   </div>
 </template>
 
--- src/components/HeaderSpotify.vue
+++ src/components/HeaderSpotify.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    ciao
+  <div class="header">
+    <div class="image-logo flex">
+        <a href="#">
+            <img src="@/assets/logo.png" alt="logo-brand">
+        </a>
+    </div>
   </div>
 </template>
 
@@ -12,5 +16,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/variables.scss';
 
+.header {
+    height: 60px;
+    background-color: $secondColor;
+
+    .image-logo {
+        padding: 10px 20px;
+
+        img {
+            width: 40px;
+        }
+    }
+}
 </style>
