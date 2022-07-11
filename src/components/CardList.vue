edf793019d0b79b0d74655465991db413f69a0d9
zhLisha <lisazhou1709@gmail.com>
2022-07-11 21:58:39 +0200

    Filter by author

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -7,10 +7,10 @@
     <div v-if="isLoading" >
         <!-- Option Chose by Genre -->
         <div class="select-genre">
-            <SelectGenre @selectGenre="currentGenre"/>
+            <SelectGenre @selectGenre="currentGenre" @selectAuthor="currentAuthor" :authorsArr="cardList"/>
         </div>
         <div class="cards flex">
-            <CardComponent v-for="(card) in filteredGenre" :key="card.id" :cardDetails="card" />
+            <CardComponent v-for="(card) in filteredSong" :key="card.id" :cardDetails="card" />
         </div>
     </div>
 
@@ -24,7 +24,7 @@
 import axios from 'axios';
 import CardComponent from './CardComponent.vue'
 import LoadingPage from './LoadingPage.vue'
-import SelectGenre from './SelectGenre.vue'
+import SelectGenre from './SearchSelect.vue'
 
 export default {
     name: 'CardList',
@@ -39,20 +39,29 @@ export default {
             url: 'https://flynn.boolean.careers/exercises/api/array/music',
             cardList: [],
             isLoading: false,
-            searchedGenre: 'All'
+            searchedGenre: 'All Genre',
+            searchedAuthor: 'All Authors'
         }
     },
 
     computed: {
-        filteredGenre() {
-            if(this.searchedGenre === 'All') {
+        filteredSong() {
+            if(this.searchedGenre === 'All Genre' && this.searchedAuthor === 'All Authors') {
                 return this.cardList;
+            } else if(this.searchedGenre !== 'All Genre' && this.searchedAuthor === 'All Authors') {
+                 return this.cardList.filter((element) => {
+                    return element.genre.includes(this.searchedGenre)
+                });
+            } else if(this.searchedAuthor !== 'All Authors' && this.searchedGenre === 'All Genre'){
+                return this.cardList.filter((element) => {
+                    return element.author.includes(this.searchedAuthor)
+                });
             }
 
             return this.cardList.filter((element) => {
-                return element.genre.includes(this.searchedGenre)
-            });
-        }
+                return element.genre.includes(this.searchedGenre) && element.author.includes(this.searchedAuthor)
+            })
+        }     
     },
 
     methods: {
@@ -67,6 +76,10 @@ export default {
 
         currentGenre(genre) {
             this.searchedGenre = genre;
+        },
+
+        currentAuthor(author) {
+            this.searchedAuthor = author;
         }
     },
 
@@ -84,7 +97,6 @@ export default {
 
 .select-genre {
     padding-top: 50px;
-
 }
 
 </style>
---
+++ src/components/SearchSelect.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="flex input-wrapper">
+    <!-- Filter by Music Genre -->
+    <label class="flex" for="genre"> 
+      <span>FILTRA PER GENERI</span>
+      <select v-model="selectedGenre" @change.prevent="changeGenre()" name="genre" id="genre">
+        <option disabled selected>Genre</option>
+        <option value="All Genre">All Genre</option>
+        <option value="Rock">Rock</option>
+        <option value="Pop">Pop</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Metal">Metal</option>
+      </select>
+    </label>
+
+    <!-- Filter by Author -->
+    <label class="flex author" for="author">
+      <span>FILTRA PER AUTORE</span>
+      <select v-model="selectedAuthor" id="author" name="author" @change.prevent="changeAuthor()" >
+        <option disabled selected>Authors</option>
+        <option value="All Authors">All Authors</option>
+        <option v-for="(author, index) in authorsArr" :key="index"  :value="author.author">{{author.author}}</option>
+      </select>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectGenre',
+    data() {
+      return {
+        selectedGenre: "All Genre",
+        selectedAuthor: "All Authors"
+      }
+    },
+
+    props: {
+      authorsArr: Array
+    },
+
+    methods: {
+      changeGenre() {
+        this.$emit('selectGenre', this.selectedGenre)
+        console.log(this.selectedGenre);
+      },
+
+      changeAuthor() {
+        this.$emit('selectAuthor', this.selectedAuthor)
+
+        console.log(this.selectedAuthor);
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+
+.input-wrapper {
+  justify-content: space-evenly;
+
+  label {
+    flex-direction: column;
+    align-items: center;
+    
+    span {
+      padding-bottom: 20px;
+      color: $mainTextColor;
+      font-size: 20px;
+    }
+
+    select {
+    padding: 5px 150px 5px 0;
+    width: 30%;
+    }
+  }
+}
+
+</style>
